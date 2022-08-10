d218d28da1853d8d197973bfa2dc834dd720e471
Oleg Sazonov <sazonov1994@gmail.com>
2022-08-10 17:04:26 +0300

    Add OptimalNumberOfStops Another one task of greedy algorithms

--- src/Algorythms/GreedyAlghorythms/MaxDigitValue.java
+++ src/Algorythms/GreedyAlghorythms/MaxDigitValue.java
@@ -1,9 +1,9 @@
-package Algorythms;
+package Algorythms.GreedyAlghorythms;
 
 import java.util.Arrays;
 import java.util.Collections;
 
-public class GreedyAlgorythms {
+public class MaxDigitValue {
     public static void main(String[] args) {
         int[] digits = {3, 1, 7, 9, 9, 5};
         System.out.println(maxNumberOfDigits(digits));
@@ -13,9 +13,9 @@ public class GreedyAlgorythms {
 
         return String.join("", // склеивает элементы массива при помощи разделителя (в кавычках)
                 Arrays.stream(digits).boxed()// массив в поток + обернуть примитив в класс обертку
-                .sorted(Collections.reverseOrder())// сортировка принимает компаратор, поэтому, и оборачиваю примитив
-                .map(String::valueOf)// привести числа в строки
-                .toArray(String[]::new));// перевести stream в массив строк
+                        .sorted(Collections.reverseOrder())// сортировка принимает компаратор, поэтому, и оборачиваю примитив
+                        .map(String::valueOf)// привести числа в строки
+                        .toArray(String[]::new));// перевести stream в массив строк
 //        String result = "";
 //        Arrays.sort(digits);
 //        for (int i = digits.length - 1; i >= 0; i--)
